e1734feef9caefb00b6cc9d8d533a2bf34c1909d
Maria Castro <mariacastro@aquaporservicos.pt>
2018-12-05 22:05:40 +0000

    better code a bit

--- weather.rb
+++ weather.rb
@@ -5,17 +5,17 @@ require 'byebug'
 
 class Weather
 
-  def give_info(action, city)
+  def give_info(request, city)
     if cities_links.include? city
-      if action == '-av_min'
-        @avg = 4
-        puts temperatures(city, 'min')
-      elsif action == '-av_max'
-        @avg = 5
-        puts temperatures(city, 'max')
-      elsif action == '-today'
-        @values = { min: 4, max: 5, wind: 9, symb_del_tiempo: 10 }
-        puts temperature(city)
+      if request == '-av_min'
+        id = 4
+        puts temperatures(city, id, split('_').last)
+      elsif request == '-av_max'
+        id = 5
+        puts temperatures(city, id, split('_').last)
+      elsif request == '-today'
+        ids = { min: 4, max: 5, wind: 9, symb_del_tiempo: 10 }
+        puts temperature(city, ids)
       else
         puts 'Por favor inserte una opción correcta:  [av_max | av_min | today]'
       end
@@ -52,24 +52,24 @@ class Weather
     cities
   end
 
-  def temperatures(city, kind)
+  def temperatures(city, id, kind)
     # array of temperatures (min or max)
     xml_doc = parse(cities_links[city])
     temperatures_arr = []
-    xml_doc.xpath("//var[icon=#{@avg}]/data/forecast").each do |forecast|
+    xml_doc.xpath("//var[icon=#{id}]/data/forecast").each do |forecast|
       temperatures_arr << forecast.xpath('@value').text.to_i
     end
     average(temperatures_arr, kind)
   end
 
-  def temperature(city)
+  def temperature(city, ids)
     # data about today's temperature (min and max)
     temp_info = {}
     xml_doc = parse(cities_links[city])
-    temp_info[:min_temp] = xml_doc.xpath("//var[icon=#{@values[:min]}]/data/forecast[@data_sequence = '#{day_adjusted}']/@value").text.to_i
-    temp_info[:max_temp] = xml_doc.xpath("//var[icon=#{@values[:max]}]/data/forecast[@data_sequence = '#{day_adjusted}']/@value").text.to_i
-    temp_info[:winds] = xml_doc.xpath("//var[icon=#{@values[:wind]}]/data/forecast[@data_sequence = '#{day_adjusted}']/@value").text
-    temp_info[:detailes_explan] = xml_doc.xpath("//var[icon=#{@values[:symb_del_tiempo]}]/data/forecast[@data_sequence = '#{day_adjusted}']/@value").text
+    temp_info[:min_temp] = xml_doc.xpath("//var[icon=#{ids[:min]}]/data/forecast[@data_sequence = '#{day_adjusted}']/@value").text.to_i
+    temp_info[:max_temp] = xml_doc.xpath("//var[icon=#{ids[:max]}]/data/forecast[@data_sequence = '#{day_adjusted}']/@value").text.to_i
+    temp_info[:winds] = xml_doc.xpath("//var[icon=#{ids[:wind]}]/data/forecast[@data_sequence = '#{day_adjusted}']/@value").text
+    temp_info[:detailes_explan] = xml_doc.xpath("//var[icon=#{ids[:symb_del_tiempo]}]/data/forecast[@data_sequence = '#{day_adjusted}']/@value").text
     "Hoy, la temperatura mínima es #{temp_info[:min_temp]}C y la temperatura máxima es #{temp_info[:max_temp]}C. Con #{temp_info[:winds]} y #{temp_info[:detailes_explan]}"
   end
 
